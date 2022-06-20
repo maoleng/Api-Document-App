cb6548997c14346fc1611d57bd946951ddb076b0
Bùi Hữu Lộc <feature451@gmail.com>
2022-06-20 08:59:43 +0700

    Update left-sidebar.blade.php

--- resources/views/layout/left-sidebar.blade.php
+++ resources/views/layout/left-sidebar.blade.php
@@ -3,10 +3,11 @@
     <!-- LOGO -->
     <a href="{{route('api.index')}}" class="logo text-center logo-light">
         <span class="logo-lg">
-            <img src="{{asset('assets/images/logo.png')}}" alt="" height="16">
+{{--            <img src="{{asset('assets/images/logo.png')}}" alt="" height="16">--}}
+            <h2>Api Document App</h2>
         </span>
         <span class="logo-sm">
-            <img src="{{asset('assets/images/logo_sm.png')}}" alt="" height="16">
+{{--            <img src="{{asset('assets/images/logo_sm.png')}}" alt="" height="16">--}}
         </span>
     </a>
 
